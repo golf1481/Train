bd76aec65e319eab9c206304fe5223c101516f2c
golf1481 <123652132+golf1481@users.noreply.github.com>
2023-09-08 10:09:19 +0700

    Update arduino.ino

--- public/arduino.ino
+++ public/arduino.ino
@@ -1,75 +1,133 @@
+#include <Arduino.h>
+#if defined(ESP32) || defined(PICO_RP2040)
 #include <WiFi.h>
-#include <WebServer.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#endif
 
-const char* ssid = "MIC_2.4GHz";
-const char* password = "999999999";
+#include <Firebase_ESP_Client.h>
 
-WebServer server(80);   // พร้อมสร้างเว็บเซิร์ฟเวอร์
+#include <addons/TokenHelper.h>
 
-void setup() 
+#include <addons/RTDBHelper.h>
+#include <Wire.h>
+#include "Adafruit_SHT4x.h"
+const int SCLpin = 40;
+const int SDApin = 41;
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+
+#define WIFI_SSID "MIC_2.4GHz"
+#define WIFI_PASSWORD "999999999"
+
+#define API_KEY "AIzaSyCL1c8gSblusSfFoePq0HHUkyCfzEcxdZE"
+
+#define DATABASE_URL "cucumber-20a25-default-rtdb.firebaseio.com/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
+#define USER_EMAIL "teeraponete148@gmail.com"
+#define USER_PASSWORD "123456"
+
+// Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+
+unsigned long count = 0;
+
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+WiFiMulti multi;
+#endif
+
+unsigned long lastMsg = 0;
+
+void setupwifi()
 {
-  Serial.begin(9600);
-  WiFi.begin(ssid, password);   // เชื่อมต่อ WiFi
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+  multi.addAP(WIFI_SSID, WIFI_PASSWORD);
+  multi.run();
+#else
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+#endif
+  Serial.print("Connecting to Wi-Fi");
+  unsigned long ms = millis();
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+    if (millis() - ms > 10000)
+      break;
+#endif
   }
-
-  Serial.println("Connected to WiFi");
-  Serial.println(WiFi.localIP());    // แสดง IP address ของบอร์ด Arduino
-
-  server.on("/", []() {
-    String html = "<html><body>";
-    html += "<h1>ModbusRTU</h1>";
-    html += "<form>";
-    for (int i = 0; i < 10; i++) {
-      html += "<label for=\"Input" + String(i) + "\">Input " + String(i) + ":</label>";
-      html += "<input type=\"text\" id=\"input" + String(i) + "\" name=\"input" + String(i) + "\"><br><br>";
-    }
-    html += "<button type=\"button\" onclick=\"sendData()\">Submit</button>";
-    html += "</form>";
-    html += "<script>";
-    html += "function sendData() {";
-    html += "  var inputs = [];";
-    for (int i = 0; i < 10; i++) {
-      html += "  var input" + String(i) + " = document.getElementById('input" + String(i) + "').value;";
-      html += "  inputs.push(input" + String(i) + ");";
-    }
-    html += "  var xhttp = new XMLHttpRequest();";
-    html += "  xhttp.onreadystatechange = function() {";
-    html += "    if (this.readyState == 4 && this.status == 200) {";
-    html += "      console.log(this.responseText);";
-    html += "    }";
-    html += "  };";
-    html += "  xhttp.open(\"GET\", \"/submit?input0=\" + inputs[0]";
-    for (int i = 1; i < 10; i++) {
-      html += "+\"&input" + String(i) + "=\" + inputs[" + String(i) + "]";
-    }
-    html += ", true);";
-    html += "  xhttp.send();";
-    html += "}";
-    html += "</script>";
-    html += "</body></html>";
-    server.send(200, "text/html", html);
-  });
-
-  server.on("/submit", []() {
-    for (int i = 0; i < 10; i++) {
-      if (server.argName(i).startsWith("input")) {
-        String value = server.arg(i);
-        Serial.print("Input " + String(i) + ": ");
-        Serial.println(value);
-      }
-    }
-    server.send(200, "text/plain", "OK");
-  });
-
-
-
-  server.begin();   // เริ่มต้นเซิร์ฟเวอร์
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+  config.api_key = API_KEY;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+  config.database_url = DATABASE_URL;
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+#if defined(ESP8266)
+  fbdo.setBSSLBufferSize(2048 /* Rx buffer size in bytes from 512 - 16384 */, 2048 /* Tx buffer size in bytes from 512 - 16384 */);
+#endif
+  fbdo.setResponseSize(2048);
+  Firebase.begin(&config, &auth);
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+  config.wifi.clearAP();
+  config.wifi.addAP(WIFI_SSID, WIFI_PASSWORD);
+#endif
+  Firebase.reconnectWiFi(true);
+  Firebase.setDoubleDigits(5);
+  config.timeout.serverResponse = 10 * 1000;
 }
 
-void loop() {
-  server.handleClient();   // รอรับและตอบกลับคำขอจาก client
+void setup()
+{
+  Serial.begin(115200);
+  setupwifi();
+  Wire1.begin(SDApin, SCLpin);
+  sht4.begin(&Wire1);
+  Serial.println("Adafruit SHT4x test");
+  if (! sht4.begin(&Wire1)) {
+    Serial.println("Couldn't find SHT4x");
+    while (1) delay(1);
+  }
+  Serial.println("Found SHT4x sensor");
+  Serial.print("Serial number 0x");
+  Serial.println(sht4.readSerial(), HEX);
+}
+int count1;
+void loop()
+{
+  float temp1, hum1;
+  unsigned long now = millis();
+  if (now - lastMsg > 5000 )
+  {
+    lastMsg = now;
+    sensors_event_t humidity, temp;
+    sht4.getEvent(&humidity, &temp);
+    Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
+    Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
+    String t1 = String(temp.temperature);
+    String h1 = String(humidity.relative_humidity);
+    temp1 = t1.toFloat();
+    hum1 = h1.toFloat();
+  }
+
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0) )
+  {
+    count1++;
+    sendDataPrevMillis = millis();
+    if (temp1 > 0.001 && hum1 > 0.001)
+    {
+      Serial.printf("Set temp... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/test/temp"), temp1) ? "ok" : fbdo.errorReason().c_str());
+      Serial.printf("Set humidity... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/test/hum"), hum1) ? "ok" : fbdo.errorReason().c_str());
+      Serial.printf("Set humidity... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/test/timestamp"), count1) ? "ok" : fbdo.errorReason().c_str());
+    }
+    
+  }
 }
